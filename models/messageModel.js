f677bfef2040cb8c1f2a5c0d566f4db659ee332d
neelgeek <neelbhave91@gmail.com>
2018-04-29 22:36:14 +0530

    Front end changes

--- models/messageModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatSchema = mongoose.Schema({
-    name: String,
-    message: String
-});
-
-
-module.exports = mongoose.model('messages', ChatSchema);
